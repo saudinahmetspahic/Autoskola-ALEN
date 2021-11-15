9bdaf9e620ed4231694ca56a0784dc5051776c5e
Saudin Ahmetspahić <saudin_ah_hotmail.com>
2021-11-15 22:37:58 +0100

    15.11.2021 / 2

--- src/components/Kontakt/Kontakt.js
+++ src/components/Kontakt/Kontakt.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Kontakt.css'
 
+import kontakt__img from '../../images/contact-us-banner.jpg'
+
 function Kontakt() {
     return (
         <div className="kontakt">
-            
+            <div className="kontakt__header">
+                <img className="kontakt__header__image" src={kontakt__img} alt="" />
+                <p className="kontakt__header__description">Ukoliko imate dodatnih pitanja ispunite formu i odgovorit ćemo u najbržem mogućem roku.</p>
+            </div>
+            <div className="kontakt__content">
+                <div className="kontakt__obrazac">
+                    <h3 className="kotakt__obrazac__header">Kontakt obrazac</h3>
+                    <p className="kotakt__obrazac__description">Kontaktirati nas možete putem kontakt obrasca ispod ili putem navedenih kontakt informacija.</p>
+                    <form className="kontakt__obrazac__form" action="">
+                        <input type="text" className="kontakt__obrazac__form__input" placeholder="Ime" />
+                        <input type="text" className="kontakt__obrazac__form__input" placeholder="Prezime" />
+                        <input type="text" className="kontakt__obrazac__form__input" placeholder="Broj telefona" />
+                        <input type="text" className="kontakt__obrazac__form__input" placeholder="Email" />
+                        <textarea type="text" className="kontakt__obrazac__form__input kontakt__obrazac__form__input--textarea" ></textarea>
+                        <button className="btn kontakt__obrazac__form__btn">Pošalji</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
