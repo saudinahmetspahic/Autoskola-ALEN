49c62529c8d7b116b68cfaac385a72b81534973e
Saudin Ahmetspahić <saudin_ah_hotmail.com>
2022-01-22 18:04:08 +0100

    22.1.2022 fixed download

--- src/components/Testovi/Testovi.js
+++ src/components/Testovi/Testovi.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import { AiOutlineDownload } from 'react-icons/ai'
 import './Testovi.css'
 
 import file_1 from "../../files/katalog pitanja oblast II.pdf"
 import file_2 from "../../files/katalog pitanja oblast III Prepravljeno.pdf"
 import file_3 from "../../files/novi_katalog_sa_odgovorima(2).pdf"
 
+var fileTitles = [
+    { Title: "Oblast I" },
+    { Title: "Oblast II" },
+    { Title: "Oblast III" }
+];
+
 function Testovi() {
-    const [doc, setDoc] = useState(file_2)
+    const [doc, setDoc] = useState(file_1)
+    const [docTitles, setDocTitles] = useState(fileTitles[0].Title)
 
     const [width, setWidth] = useState(window.innerWidth);
 
@@ -20,24 +28,50 @@ function Testovi() {
         }
     }, []);
 
-    const isMobile = width <= 768;
+    const setDocument = (idx) => {
+        switch (idx) {
+            case 1: {
+                setDoc(file_1);
+                setDocTitles(fileTitles[0].Title);
+                break;
+            }
+            case 2: {
+                setDoc(file_2);
+                setDocTitles(fileTitles[1].Title);
+                break;
+            }
+            case 3: {
+                setDoc(file_3);
+                setDocTitles(fileTitles[2].Title);
+                break;
+            }
+            default: {
+                setDoc(file_1);
+                setDocTitles(fileTitles[0].Title);
+            }
+        }
 
+    }
+
+    const isMobile = width <= 768;
     return (
         <div className="testovi">
             <iframe title="testovi" className="testovi__frame" src="https://spesdriver.com/ba/test/embed"></iframe>
             <div className="testovi_pdf_files">
                 <p className="testovi_pdf_files_description">Teorijska pitanja za polaganje vozačkog ispita.</p>
+                {isMobile &&
+                    <p className='testovi_pdf_files_select'>Odaberite file</p>
+                }
                 <div className="testvi_pdf_files__bts">
-                    <button onClick={() => setDoc(file_1)} className="btn-tab">Teorija - Oblast I</button>
-                    <button onClick={() => setDoc(file_2)} className="btn-tab">Teorija - Oblast II</button>
-                    <button onClick={() => setDoc(file_3)} className="btn-tab">Teorija - Oblast III</button>
+                    <button onClick={() => setDocument(1)} className="btn-tab">Teorija - Oblast I</button>
+                    <button onClick={() => setDocument(2)} className="btn-tab">Teorija - Oblast II</button>
+                    <button onClick={() => setDocument(3)} className="btn-tab">Teorija - Oblast III</button>
                 </div>
                 {isMobile ||
                     <iframe title='teorija' className="testovi_pdf_files_iframe" src={doc} frameborder="0"></iframe>
                 }
                 {isMobile &&
-                    <p>download</p>
-                    // <iframe className="testovi_pdf_files_iframe" src={doc} frameborder="0"></iframe> 
+                    <a className="testvi_pdf_files_download_btn" href={doc} download="Teorija.pdf" ><AiOutlineDownload /> Preuzmi - {docTitles}</a>
                 }
 
 
